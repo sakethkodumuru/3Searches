5dc127f94d0d19279150fbf10bbb824a20c5a628
PHANI TULASI BATCHU <123003190@sastra.ac.in>
2020-09-25 13:09:04 +0530

    Added common input statements

--- Searches.py
+++ Searches.py
@@ -1,1 +1,10 @@
 Print("Welcome to Searching Techniques by Bargav , Phani and Saketh")
+a = int(input("enter the no. of elements "))
+lst =[]
+l= [0]*a
+max_trails = 3
+for i in range(0,a):
+    ele = int(input("enter ur element "))
+    lst.append(ele)
+
+s =int(input("enter the element to be searched "))
